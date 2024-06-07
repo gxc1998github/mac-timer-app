0541a2befd6f27d0d800e51ebf333a6744386b48
Daniel Lip <116948520+gxc1998github@users.noreply.github.com>
2024-06-07 23:52:45 +1200

    First commit

---
+++ MenuBarTimer/MenuBarTimer/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  MenuBarTimer
+//
+//  Created by Daniel on 07/06/2024.
+//
+
+import Foundation
---
+++ MenuBarTimer/MenuBarTimer/MenuBarTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuBarTimerApp.swift
+//  MenuBarTimer
+//
+//  Created by Daniel on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuBarTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
